43ceb6b5554a6509152be3641c0d04ee9f7222c8
Semyon Yanichev <janchevsem@mail.ru>
2026-05-08 13:53:58 +0400

    test(product): add query count

--- tests/Feature/ProductCacheTest.php
+++ tests/Feature/ProductCacheTest.php
@@ -4,12 +4,11 @@ use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 uses(RefreshDatabase::class);
 
 it('caches products list and invalidates cache after product creation', function () {
-    Cache::flush();
-
     $category = Category::factory()->create();
 
     Product::factory()->create([
@@ -20,19 +19,34 @@ it('caches products list and invalidates cache after product creation', function
 
     $cacheKey = 'products:index:' . md5(json_encode([]));
 
+    DB::flushQueryLog();
+    DB::enableQueryLog();
+
+    Cache::flush();
+
     $this->getJson('/api/products')
         ->assertOk()
         ->assertJsonFragment([
             'name' => 'iPhone 15',
         ]);
 
+    expect(count(DB::getQueryLog()))->toBe(1);
+
+    DB::flushQueryLog();
+
     expect(Cache::tags('products')->has($cacheKey))->toBeTrue();
+
+    $this->getJson('/api/products')->assertOk();
+
+    expect(count(DB::getQueryLog()))->toBe(0);
 
     Product::create([
         'name' => 'Samsung Galaxy',
         'price' => 900,
         'category_id' => $category->id,
     ]);
+
+    DB::flushQueryLog();
 
     expect(Cache::tags('products')->has($cacheKey))->toBeFalse();
 
@@ -41,6 +55,8 @@ it('caches products list and invalidates cache after product creation', function
         ->assertJsonFragment([
             'name' => 'Samsung Galaxy',
         ]);
+
+    expect(count(DB::getQueryLog()))->toBe(1);
 
     expect(Cache::tags('products')->has($cacheKey))->toBeTrue();
 });
