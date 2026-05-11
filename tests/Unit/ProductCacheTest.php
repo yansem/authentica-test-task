4630f260c4d4c3f8af72022baced4ce301900787
Semyon Yanichev <janchevsem@mail.ru>
2026-05-11 20:01:47 +0400

    test(products): check cache

--- tests/Unit/ProductCacheTest.php
+++ tests/Unit/ProductCacheTest.php
@@ -2,7 +2,6 @@
 
 use App\Queries\ProductQuery;
 use App\Services\ProductService;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
@@ -11,35 +10,14 @@ uses(TestCase::class);
 it('caches products index result', function () {
     Cache::flush();
 
-    $validated = [
-        'filter' => [
-            'name' => 'iphone',
-        ],
-        'sort_by' => 'id',
-        'sort_dir' => 'asc',
-    ];
-
-    $paginator = new LengthAwarePaginator(
-        [1, 2],
-        2,
-        15
-    );
-
     $productQuery = Mockery::mock(ProductQuery::class);
 
     $productQuery
         ->shouldReceive('index')
-        ->once()
-        ->with($validated)
-        ->andReturn($paginator);
+        ->once();
 
     $service = new ProductService($productQuery);
 
-    $result1 = $service->index($validated);
-    $result2 = $service->index($validated);
-
-    expect($result1)->toBe($result2);
-
-    expect($result1->items())->toHaveCount(2);
-    expect($result2->items())->toHaveCount(2);
+    $service->index([]);
+    $service->index([]);
 });
